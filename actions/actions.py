1d8af5b7dde8c104e6525c745503bff290561c16
Martin Jaskulla <martin.jaskulla@accenture.com>
2021-02-02 21:41:39 +0100

    Project buttons work

--- actions/actions.py
+++ actions/actions.py
@@ -15,7 +15,7 @@ from rasa_sdk.executor import CollectingDispatcher
 
 
 # Would be best to keep messages in domain.yml, because NLU + Stories also want to show them
-class ActionHelloWorld(Action):
+class ActionListProjects(Action):
 
     def name(self) -> Text:
         return "action_list_projects"
@@ -23,27 +23,33 @@ class ActionHelloWorld(Action):
     def run(self, dispatcher: CollectingDispatcher,
             tracker: Tracker,
             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-        template = 'utter_show_personal_projects'
-        buttons=[]
         projects = tracker.get_slot('projects')
+        dispatcher.utter_message(buttons=projects)
+        return []
 
-        projectPicked = len(tracker.latest_message['entities']) > 0
-        if (projectPicked):
-            template = tracker.latest_message['entities'][0]['value']
-            buttons=projects
-            for i in range(len(projects)):
-                if projects[i]['payload'].index(template) > 0:
-                    del projects[i]
-                    break
-            print('template', template)
-            print('buttons', buttons)
-            print('projects', projects)
-            dispatcher.utter_message(template=template, buttons=buttons)
-            return [SlotSet("projects", projects)]
 
-        print('template', template)
-        print('buttons', buttons)
-        print('projects', projects)
-        dispatcher.utter_message(template=template)
+class ActionShowProject(Action):
+
+    def name(self) -> Text:
+        return "action_show_project"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        projects = tracker.get_slot('projects')
+        project = tracker.latest_message['entities'][0]['value']
+
+        for i in range(len(projects)):
+            if project in projects[i]['payload']:
+                del projects[i]
+                break
+        dispatcher.utter_message(template=project, buttons=projects)
+        return [SlotSet("projects", projects)]
+
+
+        # Remove from projects
+        # Show template
+        # FollowUp action the other action. or display buttons itself?
+        # If 0 projects, reset it but show different template domain['slots']['projects']['initial_value']
 
         return []
