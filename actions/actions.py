bdceec7e670e0a71f470e9e7cbc18827cc666c74
Martin Jaskulla <martin.jaskulla@accenture.com>
2021-02-02 22:03:03 +0100

    Delete 2nd action. Need only one for the project buttons

--- actions/actions.py
+++ actions/actions.py
@@ -13,21 +13,6 @@ from rasa_sdk import Action, Tracker
 from rasa_sdk.events import SlotSet
 from rasa_sdk.executor import CollectingDispatcher
 
-
-# Would be best to keep messages in domain.yml, because NLU + Stories also want to show them
-class ActionListProjects(Action):
-
-    def name(self) -> Text:
-        return "action_list_projects"
-
-    def run(self, dispatcher: CollectingDispatcher,
-            tracker: Tracker,
-            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-        projects = tracker.get_slot('projects')
-        dispatcher.utter_message(buttons=projects)
-        return []
-
-
 class ActionShowProject(Action):
 
     def name(self) -> Text:
@@ -47,9 +32,4 @@ class ActionShowProject(Action):
         return [SlotSet("projects", projects)]
 
 
-        # Remove from projects
-        # Show template
-        # FollowUp action the other action. or display buttons itself?
         # If 0 projects, reset it but show different template domain['slots']['projects']['initial_value']
-
-        return []
